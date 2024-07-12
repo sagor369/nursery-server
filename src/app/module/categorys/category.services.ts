fbeba0d2a4ecfaf9a6a86c89ad5dabf8064b2c61
Sagor <sagor.heaking@gmail.com>
2024-07-12 11:55:03 +0600

    category services some api create

--- src/app/module/categorys/category.services.ts
+++ src/app/module/categorys/category.services.ts
@@ -1,3 +1,4 @@
+import { QueryBuilder } from "../../../builder/QueriBuilder";
 import { TPlantCategory } from "./category.interface";
 import { Categorys } from "./category.model";
 
@@ -5,20 +6,23 @@ const createCategory = async (payload: TPlantCategory) => {
   const result = Categorys.create(payload);
   return result;
 };
-const getCategory = async () => {
-  const result = ""
+const getCategory = async (query: Record<string, unknown>) => {
+  const searchField = ['name', 'description']
+  const CategoryQuery = new QueryBuilder(Categorys.find(), query).search(searchField).filter().sort()
+  const result = CategoryQuery.modelQuery
   return result;
 };
 const getSingleCategory = async (id: string,) => {
-  const result = "";
+  const result = Categorys.findById(id);
   return result;
 };
 const updateCategory = async (id: string, payload: Partial<TPlantCategory>) => {
-  const result = Categorys.create(payload);
+  const result = Categorys.findByIdAndUpdate(id, payload, {new:true, runValidators: true});
   return result;
 };
 const deleteCategory = async (id:string) => {
-  const result = ""
+
+  const result = Categorys.findByIdAndUpdate(id, {isDelete: true}, {new:true, runValidators: true})
   return result;
 };
 
---
+++ src/builder/QueriBuilder.ts
@@ -0,0 +1,56 @@
+import { FilterQuery, Query } from 'mongoose';
+
+export class QueryBuilder<T> {
+  public modelQuery: Query<T[], T>;
+  public query: Record<string, unknown>;
+
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    this.modelQuery = modelQuery;
+    this.query = query;
+  }
+  search(searchFields: string[]) {
+    const searchTerm = this?.query?.searchTerm;
+    if (searchTerm) {
+      this.modelQuery = this.modelQuery.find({
+        $or: searchFields.map(
+          (field) =>
+            ({
+              [field]: { $regex: searchTerm, $options: 'i' },
+            }) as FilterQuery<T>,
+        ),
+      });
+    }
+
+    return this;
+  }
+  filter() {
+    const queryObj = { ...this.query }; 
+
+    const excludeFields = ['searchTerm', 'sort', 'limit', 'page',];
+
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
+
+    return this;
+  }
+
+  sort() {
+    const sort =
+      (this?.query?.sort as string)?.split(',')?.join(' ') || '-createdAt';
+    this.modelQuery = this.modelQuery.sort(sort as string);
+
+    return this;
+  }
+
+  paginate() {
+    const page = Number(this?.query?.page) || 1;
+    const limit = Number(this?.query?.limit) || 12;
+    const skip = (page - 1) * limit;
+
+    this.modelQuery = this.modelQuery.skip(skip).limit(limit);
+
+    return this;
+  }
+
+}
